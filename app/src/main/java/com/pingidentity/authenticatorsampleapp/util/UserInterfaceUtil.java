6c060124aadf0e3e4a7ef1559a2e3ccb0f7be30d
Jenkins-CI-Push <example@pingidentity.com>
2021-04-05 08:45:26 +0000

    Release of version 1_150_48

--- app/src/main/java/com/pingidentity/authenticatorsampleapp/util/UserInterfaceUtil.java
+++ app/src/main/java/com/pingidentity/authenticatorsampleapp/util/UserInterfaceUtil.java
@@ -1,12 +1,11 @@
 package com.pingidentity.authenticatorsampleapp.util;
 
 import android.content.Context;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-
-
 import com.pingidentity.authenticatorsampleapp.R;
 
 /*
@@ -15,28 +14,37 @@ import com.pingidentity.authenticatorsampleapp.R;
  */
 public class UserInterfaceUtil {
 
+    public static void promptMessage(Context context, TextView textView, String text){
+        displaySlider(context, textView, text);
+        Handler handler = new Handler();
+        handler.postDelayed((Runnable) () -> {
+            hideSlider(textView);
+        }, 3000);
+    }
+
     public static void handleNetworkChange(Boolean networkOnline, Context context, TextView textView){
         if(networkOnline){
-            hideNoNetworkSlider(textView);
+            hideSlider(textView);
         }else{
-            displayNoNetworkSlider(context, textView);
+            displaySlider(context, textView, context.getString(R.string.main_screen_notification_no_network));
         }
     }
 
-    private static void hideNoNetworkSlider(TextView textView){
+    private static void hideSlider(TextView textView){
         if(textView.getVisibility()==View.GONE){
             return;
         }
         textView.setVisibility(View.GONE);
     }
 
-    private static void displayNoNetworkSlider(Context context, TextView textView){
+    private static void displaySlider(Context context, TextView textView, String text){
         if(textView.getVisibility()==View.VISIBLE){
             return;
         }
-        textView.setText(R.string.main_screen_notification_no_network);
+        textView.setText(text);
         textView.setVisibility(View.VISIBLE);
         Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_down);
         textView.startAnimation(animation);
     }
+
 }
